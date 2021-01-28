4e5e1f223ffb40ae29ed25d6b50a8fbd975ec7be
yu-naga-portfolio <78053711+yu-naga-portfolio@users.noreply.github.com>
2021-01-29 07:14:08 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+    const $submitBtn = $('#js-submit')
+    $('#form input,#form textarea').on('change', function () {
+      if (
+        $('#form input[type="text"]').val() !== "" &&
+        $('#form input[type="email"]').val() !== "" &&
+        $('#form input[type="checkbox"]').val() !== "" &&
+        $('#form #privacyCheck').prop('checked') === true
+      ) {
+        $submitBtn.prop('disabled', false);
+  
+      } else {
+        $submitBtn.prop('disabled', true);
+      }
+    });
+  
+  });
